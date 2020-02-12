2408dcd0892903f17094bc5b139dedc5258e5e14
e.pugach <e.pugach@itransition.com>
2020-02-12 18:41:08 +0300

    createLeadLWC class and update LWC

--- src/lwc/creatingLeads/creatingLeads.js
+++ src/lwc/creatingLeads/creatingLeads.js
@@ -1,4 +1,5 @@
 import { LightningElement, track } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { createRecord } from 'lightning/uiRecordApi';
 import { reduceErrors } from 'c/ldsUtils';
@@ -7,15 +8,19 @@ import FIRSTNAME_FIELD from '@salesforce/schema/Lead.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Lead.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Lead.Email';
 import COMPANY_FIELD from '@salesforce/schema/Lead.Company';
+import insertLeads from '@salesforce/apex/createLeadLWC.insertLead';
+
 
 export default class CreatingLeads extends LightningElement {
-    greeting = 'Lead';
 
-    @track leadId;
-     firstName = '';
-     lastName = '';
-     email = '';
-     company = '';
+//     @track leadId;
+     @track leadList = [{
+         Id: '',
+         firstName: '',
+         lastName : '',
+         email: ''
+     }];
+     count=0;
 
      handleFirstNameChange(event) {
         this.leadId = undefined;
@@ -29,32 +34,71 @@ export default class CreatingLeads extends LightningElement {
          this.leadId = undefined;
          this.email = event.target.value;
      }
-     createLead() {
-        const fields = {};
-        fields[FIRSTNAME_FIELD.fieldApiName] = this.firstName;
-        fields[LASTNAME_FIELD.fieldApiName] = this.lastName;
-        fields[EMAIL_FIELD.fieldApiName] = this.email;
-        fields[COMPANY_FIELD.fieldApiName] = this.lastName;
-        const recordInput = { apiName: LEAD_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-            .then( lead => {
-                this.leadId = lead.id;
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Lead created',
-                        variant: 'success',
-                    }),
-                );
-            })
-            .catch(error => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error creating record',
-                        message: reduceErrors(error).join(', '),
-                        variant: 'error',
-                    }),
-                );
-            });
-    }
+
+     addItem(){
+         console.log('Hello');
+
+         this.leadList.push ({
+             Id: '',
+             firstName: '',
+             lastName : '',
+             email: ''
+         });
+
+         console.log(this.leadList);
+     }
+
+
+     createLead(event) {
+         insertLeads({
+                 amount: this.amount,
+                 stage: 'Closed Won'
+             })
+             .then(() => {
+                 return refreshApex(this.opptiesOverAmount);
+             })
+             .catch((error) => {
+                 this.message = 'Error received: code' + error.errorCode + ', ' +
+                     'message ' + error.body.message;
+             });
+         }
+
+
+
+
+
+
+
+
+
+
+//     createLead() {
+//        const fields = {};
+//        fields[FIRSTNAME_FIELD.fieldApiName] = this.firstName;
+//        fields[LASTNAME_FIELD.fieldApiName] = this.lastName;
+//        fields[EMAIL_FIELD.fieldApiName] = this.email;
+//        fields[COMPANY_FIELD.fieldApiName] = this.lastName;
+//        const recordInput = { apiName: LEAD_OBJECT.objectApiName, fields };
+//        createRecord(recordInput)
+//            .then( lead => {
+//                this.leadId = lead.id;
+//                this.dispatchEvent(
+//                    new ShowToastEvent({
+//                        title: 'Success',
+//                        message: 'Lead created',
+//                        variant: 'success',
+//                    }),
+//                );
+//            })
+//            .catch(error => {
+//                this.dispatchEvent(
+//                    new ShowToastEvent({
+//                        title: 'Error creating record',
+//                        message: reduceErrors(error).join(', '),
+//                        variant: 'error',
+//                    }),
+//                );
+//            });
+//    }
+
 }
